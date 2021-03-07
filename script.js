9f63025060fbb7e76eb381823a2b942bb8410b3e
CosminM07 <73910125+CosminM07@users.noreply.github.com>
2021-03-07 20:19:41 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -5,10 +5,15 @@ let lostMessage = document.getElementById('lostMessage');
 let playing =0;
 
 
+
+let div = document.getElementById('div');
+
+
 document.querySelector('.go').addEventListener('click', function playGame() {
   playing++;
   if(playing === 1){
-  const guess = Number(document.querySelector('.number').value);
+  
+  const guess = Number(document.querySelector('#number').value);
   
   startMessage.removeAttribute("hidden");
   function createButtons() {
@@ -16,7 +21,7 @@ document.querySelector('.go').addEventListener('click', function playGame() {
   const button = document.createElement("button");
   button.innerHTML = "Button"+ i;
   button.id=i;
-  document.body.appendChild(button);
+  div.appendChild(button);
   console.log(button);}}
  
   for (let i=0; i<guess;i++){
@@ -24,27 +29,31 @@ document.querySelector('.go').addEventListener('click', function playGame() {
     console.log(array_btn[i]);}
     console.log('Length:' + (array_btn.length));
     const randomBtn = array_btn[Math.floor(Math.random() * array_btn.length)];
-    console.log(randomBtn);
+    console.log('teast',randomBtn,document.getElementById(parseInt(randomBtn)));
   
     createButtons();
+
   
   
   document.getElementById(parseInt(randomBtn)).addEventListener('click', function(){
      winMessage.removeAttribute("hidden");
-   
+     
   });
 }
 
   document.querySelector('.playAgain').addEventListener('click', function() {
     playing=0;
-    
-    function removeAllChildNodes(parent) {
-      while (parent.firstChild) {
-          parent.removeChild(parent.firstChild);
-      }
+    array_btn = [];
+    console.log(array_btn);
+    startMessage.setAttribute("hidden", true);
+    winMessage.setAttribute("hidden", true);
+    document.getElementById('number').value = null;
+    var div=document.getElementById("div");
+    while (div.hasChildNodes()) {
+      div.removeChild(div.childNodes[0]);
     }
-    const container = document.getElementById('script');
-    removeAllChildNodes(container);
+    
+    
    
     
     console.log('play agian');
